a302c9438ec94da12990e1f3648abd36de41ff00
haliespaige <halie.romasco@gmail.com>
2023-06-09 14:50:51 -0700

    saving work

--- src/pages/DogNew.js
+++ src/pages/DogNew.js
@@ -1,9 +1,49 @@
 import React from "react"
+import { Form, FormGroup, Label, Input, Button } from "reactstrap"
+import { useNavigate } from "react-router-dom"
 
 const DogNew = () => {
+  const [newDog, setNewDog] = useState 
   return(
     <>
        <h1>Dog New</h1>
+      <Form>
+        <FormGroup row>
+          <Label
+            for="dog-name"
+          >
+            Enter Dog Name
+          </Label>
+          <Col sm={10}>
+            <Input
+              id="dog-name"
+              name="name"
+              placeholder="Dog Name"
+              type="text"
+              onChange={handleChange} 
+              value={newDog.name}
+            />
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label
+            for="dog-age"
+          >
+            Enter Dog Age
+          </Label>
+          <Col sm={10}>
+            <Input
+              id="dog-age"
+              name="age"
+              placeholder="Dog Age"
+              type="text"
+              onChange={handleChange} 
+              value={newDog.age}
+            />
+          </Col>
+        </FormGroup>
+
+      </Form>
     </>
   )
 }
